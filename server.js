60aa0976a113c89bfab96370ac35cc8309f16b4d
ismaeladrian89 <ismael.sanchez1989@proton.me>
2026-01-04 18:11:50 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config()
+
+app.use('/api', require('./routes/authRoutes'))
+
+app.get('/', (req,res) => res.send('My Backend'))
+const port = process.env.PORT 
+app.listen(port, () => console.log(`Server is running on port ${port}`))
