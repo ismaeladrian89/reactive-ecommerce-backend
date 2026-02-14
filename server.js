2fc8fccb58de0e99fe1a97b874d4759a48439421
ismaeladrian89 <ismael.sanchez1989@proton.me>
2026-02-14 10:34:21 -0600

    Third commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 require('dotenv').config()
 
@@ -8,6 +10,9 @@ app.use(cors({
     origin: ['http://localhost:5173'],
     credentials: true
 }))
+
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 app.use('/api', require('./routes/authRoutes'))
 
