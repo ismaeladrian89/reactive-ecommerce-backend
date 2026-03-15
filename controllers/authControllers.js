ebeee51c6068a89d3d040cf0fd46977998038cb6
ismaeladrian89 <ismael.sanchez1989@proton.me>
2026-03-14 19:53:10 -0600

    March 14, 2026

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,41 +2,47 @@ const adminModel = require('../models/adminModel')
 const { responseReture } = require('../utiles/response')
 const { createToken } = require('../utiles/tokenCreate')
 const bcrypt = require('bcrypt')
-
-class authControllers{
-    admin_login = async(req,res) => {
-        const {email,password} = req.body
+ 
+class authControllers {
+    admin_login = async (req, res) => {
+        let { email, password } = req.body
+ 
         try {
-            const admin = await adminModel.findOne({email}).select('+password')
-             console.log(admin)
-            if (admin) {
-                const match = await bcrypt.compare(password, admin.password)
-                console.log(match)
-           
-                if(match){
-                    const token = await createToken({
-                        id: admin.id,
-                        role: admin.role
-                    })
-                    res.cookie('accessToken', token, {
-                        expires: new Date(Date.now() + 7*24*60*60*1000)
-                    })
-                    responseReture(res,200,{token,message: "Login Success"})
-                } else {
-                    responseReture(res,404,{error: "Password Wrong"})
-                }
-           
-            } else {
-                responseReture(res,404,{error: "Email not Found"})
+            email = email.trim().toLowerCase()
+ 
+            const admin = await adminModel.findOne({ email }).select('+password')
+ 
+            console.log("Input email:", email)
+            console.log("Admin found:", admin)
+ 
+            if (!admin) {
+                return responseReture(res, 404, { error: "Email not Found" })
             }
-            
+ 
+            const match = await bcrypt.compare(password, admin.password)
+            console.log("Password match:", match)
+ 
+            if (!match) {
+                return responseReture(res, 404, { error: "Password Wrong" })
+            }
+ 
+            const token = await createToken({
+                id: admin._id,
+                role: admin.role
+            })
+ 
+            res.cookie('accessToken', token, {
+                expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+                httpOnly: true
+            })
+ 
+            return responseReture(res, 200, { token, message: "Login Success" })
+ 
         } catch (error) {
-            responseReture(res,500,{error: error.message})
+            console.log(error)
+            return responseReture(res, 500, { error: error.message })
         }
-
-
-
     }
 }
-
-module.exports = new authControllers()
+ 
+module.exports = new authControllers() 
--- utiles/db.js
+++ utiles/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.dbConnect = async()=>{
     try {
-        await mongoose.connect(process.env.DB_URL,{useNewURLParser: true})
+        await mongoose.connect(process.env.DB_URL,{useNewUrlParser: true})
         console.log("Database connected..")
     } catch (error) {
         console.log(error.message)
